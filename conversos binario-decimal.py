f855d78655ebb4ad494811f6c2dfdede0d552b71
Angel-Ram12 <deyarmz.1412@gmail.com>
2025-06-19 15:16:17 -0600

    Add files via upload

---
+++ conversos binario-decimal.py
@@ -0,0 +1,12 @@
+print ("ingrese valor ")
+
+
+binariox1=(1,50,90,128)
+binariox0=(4,23,70,90,110)
+
+
+if valor is x1:
+       print("binario")
+
+print ("El valor binario es")
+print (resultado_suma(valores))
